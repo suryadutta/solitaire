335435dfb99af6a16b5aa6b16240f0e2f06f7798
Katy Felkner <katy.felkner@ou.edu>
2019-09-26 23:48:04 -0500

    add skeleton feature vector classes

---
+++ high_level_vector.py
@@ -0,0 +1,16 @@
+# this file should contain an implementation of a high-level state vector
+
+# tentatively, using numpy arrays for vectors
+from numpy import array
+
+class HighLevelVector:
+
+    # vector keeps track of its game and updates itself from the game
+    def __init__(self, game):
+        self.game = game
+        self.update(self.game.getGameElements())
+
+    def update(self, gameElements):
+        # this method should update the vector according to the game elements "printout" received from self.game
+        # implementation will depend on what we do for features
+        pass
