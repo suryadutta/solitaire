06b9f965abd9def641ca971da8473238fe5204de
Olivia Black <olivia.black@ou.edu>
2019-10-02 19:43:26 -0500

    Fuck this

--- agent.py
+++ agent.py
@@ -1,36 +1,34 @@
 from solitaire import Game
+from high_level_vector import HighLevelVector
+from low_level_vector import LowLevelVector
+from numpy import array
+import numpy as np
 
 class agent:
-    #fill in Q with arbitrary (?) values
-    #let's just do all 0's
-    def init_Q():
-
-
-
-    #I have no idea what this is going to do
-    def get_Q(state,action):
-
-        if Game.checkIfCompleted() or Game.checkIfOutOfMoves():
-            return 0
-
-
-    #Function to make a move given current state and action
-    #@return next state and reward
-    def make_move(s,a):
-        return Game.make_move(s,a)
 
     #Function to choose what action to take next, depending on next state and epsilon
     #@return next action
-    def epsilon_greedy(s,epsilon):
+    def epsilon_greedy(state,epsilon):
 
         #find all valid moves that can be made- some kind of vector
-        possible_moves = Game.getPossibleMoves()
+        possible_moves = state.getPossibleMoves()
 
         #get rewards corresponding to all possible moves
-        #choose move with hightest reward
+        #TODO - don't actually want to make the move, just get what the reward would be
+        rewards=[]
+        for action in possible_moves:
+            rewards.append(state.make_move(state,action))
 
-        #do random move 1-epsilon % of the time
+        #get random number between 1-10
+        #if random number > epsilon*10 choose an action at random
+        random_chance = np.random.randint(1,11)
+        if random_chance > epsilon*10:
+            random_choice = np.random.randint(0,len(possible_moves))
+            return possible_moves[random_choice]
 
+        #else choose move with hightest reward
+        max_index = rewards.index(max(rewards))
+        return possible_moves[max_index]
 
     """
     @param alpha- learning rate
@@ -48,8 +46,11 @@ class agent:
         actions=["moveBetweenPiles","moveDeckToPile","movePileToBlock","moveBlockToPile",
                  "recycleDeck","drawDeck"]
 
-        #initialize Q as a matrix (?) size num actions x num states in vector
-        Q = init_Q()
+        #Get initial Q, depending on whether high or low level features
+        if high_level:
+            features = HighLevelVector
+        else:
+            features = LowLevelVector
 
         for game in range(num_games):
 
@@ -58,23 +59,25 @@ class agent:
             total_score = 0
             moves = 0
 
-            #get initial state based on new game
-            state = Game.newGame(high_level)
+            #get initial state, action, Q, based on new game
+            state = Game()
             action = epsilon_greedy(state,epsilon)
-            Q = get_Q(state,action)
+            Q = features.get_Q(state,action)
 
             while moves < max_moves:
 
                 #make a move, update state and total score
-                reward, next_state = make_move(state,action)
+                reward, next_state = state.make_move(state,action)
                 total_score += reward
                 moves += 1
 
+                #get next action using epsilon greedy and corresponding Q
                 next_action = epsilon_greedy(next_state,epsilon)
-                next_Q = get_Q(next_state,next_action)
+                next_Q = features.get_Q(next_state,next_action)
 
-                #Update Q, state, action
+                #Update Q, feature weights, state, action
                 Q += alpha*[reward+(gamma*next_Q-Q)]
+                features.update_weights()
                 state = next_state
                 action = next_action
 
--- low_level_vector.py
+++ low_level_vector.py
@@ -2,20 +2,27 @@
 
 # tentatively, using numpy arrays for vectors
 from numpy import array
+from solitaire import Game
 
 class LowLevelVector:
     # vector keeps track of its game and updates itself from the game
     #numpy array representation
     #1-4: number face-up cards in block piles
     #5-11: number face-up cards in each play pile
+    #keep corresponding weight array of feature vector size + 1
     def __init__(self, game):
         self.game = game
         lowLevelVector = numpy.array([0,0,0,0,1,1,1,1,1,1,1])
+        lowLevelWeights = numpy.array([1,1,1,1,1,1,1,1,1,1,1,1])
         self.update(self.game.getGameElements())
 
-    def update(self, gameElements):
+    #update feature vector and weight array
+    def update_features(self, state, action):
         # this method should update the vector according to the game elements "printout" received from self.game
         # implementation will depend on what we do for features
+
+        next_state = state.make_move(state,action)
+        gameElements = next_state.getGameElements()
 
         blockPiles = gameElements(1)
         i=0
@@ -29,3 +36,21 @@ class LowLevelVector:
             self.lowLevelVector[i] = len(pile.getFlippedCards())
 
         pass
+
+    #TODO update weight array with gradient stuff
+    def update_weights(self):
+        pass
+
+    #get Q using linear function approximation
+    def get_Q(self,state,action):
+
+        update_features(state,action)
+
+        Q=lowLevelWeights[0]
+        for i in range(lowLevelVector):
+            Q += lowLevelVector[i]*lowLevelWeights[i+1]
+
+        return Q
+
+
+
