e99be76af6d0b89ad741429a77092972cccc9d6b
Olivia Black <olivia.black@ou.edu>
2019-10-05 10:54:43 -0500

    The simulator finally works

--- solitaire.py
+++ solitaire.py
@@ -28,9 +28,10 @@ class Game:
 
     def getGameElements(self):
         returnObject = {
-            "deck": str(self.deck),
             "playPiles": [str(pile) for pile in self.playPiles],
-            "blockPiles": {suit: str(pile) for suit, pile in self.blockPiles.items()}
+            "blockPiles": {suit: str(pile) for suit, pile in self.blockPiles.items()},
+            "trash pile up": ", ".join([str(card) for card in self.trashPileUp]),
+            "trash pile down": ", ".join([str(card) for card in self.trashPileDown])
         }
         return returnObject
 
@@ -85,12 +86,14 @@ class Game:
         if card is None:
             return False
         for pile in self.playPiles:
-            if self.checkCardOrder(pile.cards[0], card):
-                moves.append(pile)
+            if len(pile.cards)>0:
+                if self.checkCardOrder(pile.cards[0], card):
+                    moves.append(pile)
         if len(moves) > 0:
             return moves
         else:
             return False
+
 
 
 
@@ -99,20 +102,20 @@ class Game:
 
     def takeTurn(self, verbose=False):
 
-        #Pre: flip up unflipped pile end cards -> do this automatically
-        [pile.cards[0].flip() for pile in self.playPiles if len(pile.cards)>0 and not pile.cards[0].flipped]
-
         #1: check if there are any play pile cards you can play to block piles
         for pile in self.playPiles:
             if len(pile.cards) > 0 and self.addToBlock(pile.cards[0]):
                 card_added = pile.cards.pop(0)
+                if len(pile.cards) > 0 and not pile.cards[0].flipped:
+                    pile.cards[0].flip()
                 if verbose:
                     print("Adding play pile card to block: {0}".format(str(card_added)))
                 return True
 
         #2: check if cards in deck can be added
-        if self.addToBlock(self.deck.getFirstCard()):
-            card_added = self.deck.takeFirstCard()
+        if len(self.trashPileUp) > 0 and self.addToBlock(self.trashPileUp[-1]):
+            card_added = self.trashPileUp[-1]
+            self.trashPileUp.pop(-1)
             if verbose:
                 print("Adding card from deck to block: {0}".format(str(card_added)))
             return True
@@ -123,24 +126,28 @@ class Game:
                 for pile2 in self.playPiles:
                     if len(pile2.cards)>1 and pile2.cards[0].value == "K":
                         card_added = pile2.cards.pop(0)
+                        if len(pile2.cards) > 0 and not pile2.cards[0].flipped:
+                            pile2.cards[0].flip()
                         pile.addCard(card_added)
                         if verbose:
                             print("Moving {0} from Pile to Empty Pile".format(str(card_added)))
                         return True
 
-                if self.deck.getFirstCard() is not None and self.deck.getFirstCard().value == "K":
-                    card_added = self.deck.takeFirstCard()
+                if len(self.trashPileUp) > 0 and self.trashPileUp[-1].value == "K":
+                    card_added = self.trashPileUp[-1]
                     pile.addCard(card_added)
+                    self.trashPileUp.pop(-1)
                     if verbose:
                         print("Moving {0} from Deck to Empty Pile".format(str(card_added)))
                     return True
 
         #4: add drawn card to playPiles
         for pile in self.playPiles:
-            if len(pile.cards)>0 and self.deck.getFirstCard() is not None:
-                if self.checkCardOrder(pile.cards[0],self.deck.getFirstCard()):
-                    card_added = self.deck.takeFirstCard()
+            if len(pile.cards)>0 and len(self.trashPileUp) > 0:
+                if self.checkCardOrder(pile.cards[0],self.trashPileUp[-1]):
+                    card_added = self.trashPileUp[-1]
                     pile.addCard(card_added)
+                    self.trashPileUp.pop(-1)
                     if verbose:
                         print("Moving {0} from Deck to Pile".format(str(card_added)))
                     return True
@@ -160,6 +167,8 @@ class Game:
                                 if pile2_downcard_count < pile1_downcard_count:
                                     [pile2.cards.insert(0,card) for card in reversed(cards_to_transfer)]
                                     pile1.cards = pile1.cards[transfer_cards_size:]
+                                    if len(pile1.cards) > 0 and not pile1.cards[0].flipped:
+                                        pile1.cards[0].flip()
                                     if verbose:
                                         print("Moved {0} cards between piles: {1}".format(
                                             transfer_cards_size,
@@ -169,6 +178,9 @@ class Game:
                                 elif pile1_downcard_count==0 and len(cards_to_transfer) == len(pile1.cards):
                                     [pile2.cards.insert(0,card) for card in reversed(cards_to_transfer)]
                                     pile1.cards = []
+                                    if len(pile1.cards) > 0 and not pile1.cards[0].flipped:
+                                        pile1.cards[0].flip()
+
                                     if verbose:
                                         print("Moved {0} cards between piles: {1}".format(
                                             transfer_cards_size,
@@ -177,12 +189,16 @@ class Game:
                                     return True
         return False
 
-    def simulate(self, verbose=False):
+    def simulate(self,  recycles, verbose=False):
 
+        actions = self.getPossibleMoves()
         turnResult = self.takeTurn(verbose=verbose)
+        if recycles == 10:
+            print("You recycled too much")
+            return
 
         if turnResult:
-            self.simulate(verbose=verbose)
+            self.simulate(recycles,verbose=verbose)
 
         else:
             # End: draw from deck
@@ -191,14 +207,19 @@ class Game:
                 currentCard = self.trashPileDown.pop(0)
                 currentCard.flip()
                 self.trashPileUp.append(currentCard)
-                print("Flipped a card on waste pile")
-                return self.simulate(verbose=verbose)
+                print("Drawing new card: {0}".format(str(currentCard)))
+                return self.simulate(recycles,verbose=verbose)
             else:
-                if len(self.trashPileUp) < 1:
+                if len(self.trashPileUp) > 1:
                     # recycle trash
-                    self.trashPileDown.extend([self.trashPileUp.pop(0).flip() for i in range(0, len(self.trashPileUp))])
+                    self.trashPileDown = self.trashPileUp
+                    self.trashPileUp = []
+                    for i in self.trashPileDown:
+                        i.flip()
+                    #self.trashPileDown.extend([self.trashPileUp.pop(0).flip() for i in range(0, len(self.trashPileUp))])
                     print("Recycling deck")
-                    return self.simulate(verbose=verbose)
+                    recycles += 1
+                    return self.simulate(recycles,verbose=verbose)
                 elif verbose:
                     print("No more moves left!")
                 return
@@ -208,9 +229,18 @@ class Game:
     # Get all possible moves somehow
     def getPossibleMoves(self):
         actions = []
+
         # start with finding all possible moves from pile to pile
         for pile1 in self.playPiles:
             pile1_flipped_cards = pile1.getFlippedCards()
+
+            #if a pile is empty and another pile has a king
+            if len(pile1.cards) == 0:  # pile has no cards
+                for pile2 in self.playPiles:
+                    if len(pile2.cards) > 1 and pile2.cards[0].value == "K":
+                        actions.append(Action(pile2.getFlippedCards(),pile2,pile1))
+
+
             if len(pile1_flipped_cards) > 0:
                 for pile2 in self.playPiles:
                     pile2_flipped_cards = pile2.getFlippedCards()
@@ -227,21 +257,28 @@ class Game:
 
         # then find all moves from pile to block
         for pile in self.playPiles:
-            add = self.canAddToBlock(pile.cards[0])
-            if len(pile.cards) > 0 and add:
-                actions.append(Action([pile.cards[0]], pile, add))
+            if len(pile.cards) > 0:
+                add = self.canAddToBlock(pile.cards[0])
+                if add:
+                    actions.append(Action([pile.cards[0]], pile, add))
 
-        # then, look for moves from block to piles (negative reward, but technically allowed.
-        for block in self.blockPiles:
-            add = self.canMoveBlockToPile(block.cards[0])
-            if len(block.cards) > 0 and add:
-                actions.extend([Action([block.cards[0]], block, add[i]) for i in add])
+        # then, look for moves from block to piles (negative reward, but technically allowed)
+        for suit in self.suits:
+            if len(self.blockPiles[suit].cards) > 0:
+                add = self.canMoveBlockToPile(self.blockPiles[suit].cards[0])
+                if add:
+                    actions.extend([Action(self.blockPiles[suit].cards[0], self.blockPiles[suit], i.cards) for i in add])
+
+        #for block in self.blockPiles:
+        #    add = self.canMoveBlockToPile(block.cards[0])
+        #    if len(block.cards) > 0 and add:
+        #        actions.extend([Action([block.cards[0]], block, add[i]) for i in add])
 
         # then look for opportunity to draw card
         if len(self.trashPileDown) > 0:
             actions.append(Action(self.trashPileDown[0], self.trashPileDown, self.trashPileUp))
         # then, look for opportunity to recycle trash
-        if len(self.trashPileUp) < 1:
+        if len(self.trashPileDown) < 1:
             # recycle trash
             # for now, we represent this action as (None, self.trashPileUp, self.trashPileDown)
             actions.append(Action(None, self.trashPileUp, self.trashPileDown))
@@ -250,14 +287,23 @@ class Game:
 
         # trash to pile
         for pile in self.playPiles:
-            add = self.checkCardOrder(pile.cards[0], self.trashPileUp[0])
-            if len(self.trashPileUp) > 0 and add:
-                actions.append(Action(self.trashPileUp[0], self.trashPileUp, pile))
+
+            if len(self.trashPileUp) > 0:
+                if len(pile.cards)==0 and self.trashPileUp[-1].value=='K':
+                    actions.append(Action(self.trashPileUp[-1],self.trashPileUp,pile))
+
+                if len(pile.cards) > 0:
+                    add = self.checkCardOrder(pile.cards[0], self.trashPileUp[-1])
+                    if add:
+                        actions.append(Action(self.trashPileUp[-1], self.trashPileUp, pile))
 
         # trash to block
-        add = self.canAddToBlock(self.trashPileUp[0])
-        if len(self.trashPileUp) > 0 and add:
-            actions.append(Action(self.trashPileUp[0], self.trashPileUp, add))
+        if len(self.trashPileUp) > 0:
+            add = self.canAddToBlock(self.trashPileUp[-1])
+            if add:
+                actions.append(Action(self.trashPileUp[-1], self.trashPileUp, add))
+
+        return actions
 
     #Check if player is out of moves
     def checkIfOutOfMoves(self):
